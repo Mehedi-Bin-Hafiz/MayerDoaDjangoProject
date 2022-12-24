a4addee5d33a79748d4c5c6d4e73f7a6e0d812c6
mehedibinhafiz <mehedibinhafiz@gmail.com>
2022-12-24 13:51:05 +0600

    setting update

--- MayerDoaEnterprise/settings.py
+++ MayerDoaEnterprise/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-up0uktce1g1dhegcxqb@j*bz5_w@e3@3#$alt^m@nbh3v^u0z#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
